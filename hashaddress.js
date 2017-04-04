72283c6387a8ed47be1660876bb43132fa006e78
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-04-04 11:56:31 +0200

    implement operations on base64 encoded hash addresses. Use ldoc for doc. Bump version / release

--- hashaddress.js
+++ hashaddress.js
@@ -1,6 +1,98 @@
-/*
- * Hashes as addresses, and utility functions for Kademlia-like routing.
- */
+// Hashes as addresses, and utility functions for Kademlia-like routing.
+//
+
+let length = 96/6;
+let tests = {};
+// # Base64 alphabet
+let base64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+tests.TEST_base64 = () => {
+  base64[63] === '/' || throwError();
+}
+
+async function hashAddress(src) { // #
+  if(typeof src === 'string') {
+    src = ascii2buf(src);
+  }
+  let hash = await crypto.subtle.digest('SHA-256', src);
+  return btoa(buf2ascii(new Uint8Array(hash))).slice(0,length);
+}
+tests.TEST_hashAddress = async () => {
+  (await hashAddress('hello')) === 'LPJNul+wow4m6Dsq' || throwError();
+}
+
+// # `dist(a,b)`
+//
+// Calculate xor-distance between two base64 addresses.
+//
+function dist(a, b) {
+  let len = Math.min(a.length, b.length);
+  let dist = 0;
+  let i = 0;
+  for(;;) {
+    va = base64.indexOf(a[i]);
+    vb = base64.indexOf(b[i]);
+    if(va === -1 || vb === -1) {
+      return dist;
+    }
+    dist += (va ^ vb) * (2 ** (-5 - 6*i));
+    ++i;
+  }
+}
+tests.TEST_distBit = () => {
+  dist('Abracadabra', 'Abracadabra') === 0 || throwError();
+  dist('A', 'B') ===  2 ** -5 || throwError();
+}
+
+// # `distBit(a,b)`
+//
+// Get the position of the first bit that differs in a,b. This is logarithmic xor-distance.
+//
+function distBit(a, b) {
+  return Math.ceil(Math.log(1/dist(a,b)) / Math.log(2));
+}
+tests.TEST_distBit = () => {
+  distBit('A', base64[32]) === 0 || throwError();
+  distBit('A', base64[31]) === 1 || throwError();
+  distBit('AA', 'A/') === 6 || throwError();
+  distBit('A', 'A') === Infinity || throwError();
+}
+
+// # `flipBitAndRandom(addr, bitpos)`
+//
+// Create a new address, preserving the first `bitpos - 1` bits, the bit at `bitpos` is flipped, and the rest of the bits are random.
+//
+function flipBitAndRandom(addr, bitpos) {
+  let result = addr.slice(0, bitpos / 6 | 0);
+
+  let word = base64.indexOf(addr[bitpos / 6 | 0]);
+  if(word === -1) {
+    word = 0;
+  }
+  let flipBits = 64 + (Math.random() * 64) >> ((bitpos % 6) + 1);
+  word = word ^ flipBits;
+  result += base64[word];
+
+  for(let i = (bitpos / 6)+1 | 0; i < length; ++i) {
+    result += base64[Math.random() * 64 | 0];
+  }
+
+  return result;
+}
+tests.TEST_flipBitAndRandom = () => {
+  flipBitAndRandom('AAAAAA', 11).startsWith('AB') || throwError();
+  flipBitAndRandom('//////', 17).startsWith('//+') || throwError();
+  flipBitAndRandom('A', 6).startsWith('A') || throwError();
+  flipBitAndRandom('B', 5).startsWith('A') || throwError();
+  for(let i = 0; i < 10; ++i) {
+    "CD".indexOf(flipBitAndRandom('A', 4)[0]) !== -1 || throwError();
+    "EFGH".indexOf(flipBitAndRandom('A', 3)[0]) !== -1 || throwError();
+  }
+}
+
+for(let k in tests) {
+  tests[k]();
+}
+
 class HashAddress { // #
 
   constructor(o) { // ##
@@ -97,9 +189,9 @@ class HashAddress { // #
       if(a[i] !== b[i]) {
         return (2 ** (93 - i*8)) *
           (((a[i] ^ b[i]) << 23) |
-          ((a[i + 1] ^ b[i + 1]) << 15) |
-          ((a[i + 2] ^ b[i + 2]) << 7) |
-          ((a[i + 3] ^ b[i + 3]) >> 1));
+            ((a[i + 1] ^ b[i + 1]) << 15) |
+            ((a[i + 2] ^ b[i + 2]) << 7) |
+            ((a[i + 3] ^ b[i + 3]) >> 1));
       }
     }
     return 0;
@@ -166,10 +258,14 @@ class HashAddress { // #
     zero.flipBitRandomise(7).toHex().startsWith('01') || throwError();
     zero.flipBitRandomise(7+8).toHex().startsWith('0001') || throwError();
   }
-}// exports #
 
-if(typeof module !== undefined) {
-  module.exports = HashAddress;
+}
+// # Exports
+
+if(typeof module !== 'undefined') {
+  exports.HashAddress = HashAddress;
+  exports.hashAddress = hashAddress;
+  exports.TESTS = tests;
 }
 
 // # Utility functions
